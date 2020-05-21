346ed04b03f6b6177359ffa39bef83b866c4ee70
Your Name <you@example.com>
2020-05-21 16:44:01 +0900

    first commit

---
+++ Script1.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+
+function toggle() {
+    var sec = document.getElementById('sec');
+    var nav = document.getElementById('navigation');
+    var mid = document.getElementById('mida');
+    var pro = document.getElementById('prof');
+
+    sec.classList.toggle('active')
+    nav.classList.toggle('active')
+    mid.classList.toggle('active')
+    pro.classList.toggle('active')
+    }
+ 
